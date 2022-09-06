183e51fff677901024f6f7f9c237f02ba913f8ca
lioTauhid <77046554+lioTauhid@users.noreply.github.com>
2022-09-06 16:43:08 +0600

    final

--- lib/google_signIn.dart
+++ lib/google_signIn.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class GoogleAuthApi {
+  static final FirebaseAuth _auth = FirebaseAuth.instance;
   static final _googleSignIn =
       GoogleSignIn(scopes: ['https://mail.google.com/']);
+
   static Future<GoogleSignInAccount?> signIn() async {
     if (await _googleSignIn.isSignedIn()) {
       return _googleSignIn.currentUser;
@@ -11,5 +14,8 @@ class GoogleAuthApi {
     }
   }
 
-  static Future signOut() => _googleSignIn.signOut();
+  static Future signOut() async {
+    await _googleSignIn.signOut();
+    await _auth.signOut();
+  }
 }
